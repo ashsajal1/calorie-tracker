578818cd009b09831a2f076b76f61b0a53d5ae18
Ashsajal <ashsajal1@gmail.com>
2025-04-28 14:27:58 +0600

    feat: add calorie store

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,30 @@
-<template>
-  <h1>Count is {{ store.count }}</h1>
+<script setup lang="ts">
+import { useCalorieStore } from '@/stores/calorieStore';
 
-  <div class="flex gap-2">
-    <Button label="Increment" @click="store.increment" />
+const calorieStore = useCalorieStore();
+
+function add() {
+  calorieStore.addCalories(500);
+}
+
+function burn() {
+  calorieStore.burnCalories(200);
+}
+
+function reset() {
+  calorieStore.resetCalories();
+}
+</script>
+
+<template>
+  <div class="p-4">
+    <h1 class="text-xl font-bold">Calories: {{ calorieStore.totalCalories }}</h1>
+    <p>{{ calorieStore.calorieStatus }}</p>
+
+    <div class="space-x-2 mt-4">
+      <button @click="add" class="bg-green-500 px-4 py-2 text-white rounded">Add 500</button>
+      <button @click="burn" class="bg-red-500 px-4 py-2 text-white rounded">Burn 200</button>
+      <button @click="reset" class="bg-gray-500 px-4 py-2 text-white rounded">Reset</button>
+    </div>
   </div>
 </template>
-
-<script setup>
-import { useCounterStore } from "@/stores/counter";
-import { Button } from "primevue";
-
-const store = useCounterStore();
-</script>
