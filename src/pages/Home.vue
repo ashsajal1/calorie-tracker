0a6c29b45e23bd8c88ae66d45e0fc53f0a0846a7
Ashsajal <ashsajal1@gmail.com>
2025-04-28 15:26:36 +0600

    feat: Add food history table

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { useCalorieStore } from '@/stores/calorieStore';
+import { useCalorieStore } from "@/stores/calorieStore";
+import { Card, Button } from "primevue";
 
 const calorieStore = useCalorieStore();
 
@@ -18,13 +19,42 @@ function reset() {
 
 <template>
   <div class="p-4 grid place-items-center">
-    <h1 class="text-xl font-bold h-40 w-40 rounded-full p-6 bg-green-500 text-center grid place-items-center">Calories: {{ calorieStore.totalCalories }}</h1>
+    <h1
+      class="text-xl font-bold h-40 w-40 rounded-full p-6 bg-green-500 text-center grid place-items-center"
+    >
+      Calories: {{ calorieStore.totalCalories }}
+    </h1>
     <p>{{ calorieStore.calorieStatus }}</p>
 
     <div class="space-x-2 mt-4">
-      <button @click="add" class="bg-green-500 px-4 py-2 text-white rounded">Add 500</button>
-      <button @click="burn" class="bg-red-500 px-4 py-2 text-white rounded">Burn 200</button>
-      <button @click="reset" class="bg-gray-500 px-4 py-2 text-white rounded">Reset</button>
+      <button @click="add" class="bg-green-500 px-4 py-2 text-white rounded">
+        Add 500
+      </button>
+      <button @click="burn" class="bg-red-500 px-4 py-2 text-white rounded">
+        Burn 200
+      </button>
+      <button @click="reset" class="bg-gray-500 px-4 py-2 text-white rounded">
+        Reset
+      </button>
+    </div>
+  </div>
+  
+  <h2 class="text-2xl font-bold mt-8 p-3">Eating History</h2>
+
+  <div>
+    <div class="flex flex-row w-full items-center gap-2 justify-between p-3 border-b dark:border-b-gray-700">
+      <div class="flex flex-col gap-2 pb-2">
+        <p class="text-lg font-bold">Food Name</p>
+        <p class="text-sm text-gray-500 flex gap-2">
+          <span>Calories: 100</span>
+          -
+          <span>12 Dec, 2024</span>
+        </p>
+      </div>
+      <div class="flex flex-row gap-2">
+        <Button size="small" icon="pi pi-trash" severity="danger"></Button>
+        <Button size="small" icon="pi pi-pen-to-square" severity="secondary"></Button>
+      </div>
     </div>
   </div>
 </template>
