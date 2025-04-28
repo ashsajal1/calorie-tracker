4f91aeb1f198eeb2027bc3ef68d923c5c5bb325d
Ashsajal <ashsajal1@gmail.com>
2025-04-28 19:21:09 +0600

    feat: show real data

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,9 +24,7 @@ const handleAddFood = () => {
 
 <template>
   <div class="p-4 grid place-items-center">
-    <h1
-      class="text-xl font-bold h-40 w-40 rounded-full p-6 bg-green-500 text-center grid place-items-center"
-    >
+    <h1 class="text-xl font-bold h-40 w-40 rounded-full p-6 bg-green-500 text-center grid place-items-center">
       Calories: {{ todayCalories }}
     </h1>
     <p>{{ calorieStore.calorieStatus }}</p>
@@ -34,56 +32,33 @@ const handleAddFood = () => {
 
   <div class="p-3 flex justify-between items-center">
     <h2 class="text-2xl font-bold mt-8">Eating History</h2>
-    <Button
-      @click="visible = true"
-      icon="pi pi-plus"
-      severity="secondary"
-      class="mt-4"
-    />
+    <Button @click="visible = true" icon="pi pi-plus" severity="secondary" class="mt-4" />
   </div>
 
-  <Dialog
-    v-model:visible="visible"
-    :header="`Add Food`"
-    :modal="true"
-    :draggable="false"
-    :resizable="false"
-  >
+  <Dialog v-model:visible="visible" :header="`Add Food`" :modal="true" :draggable="false" :resizable="false">
     <div>
       <div class="flex flex-col gap-2">
         <InputText v-model="foodName" placeholder="Food Name" />
         <InputNumber v-model="calories" placeholder="Calories" />
-        <Button
-          class="w-full"
-          @click="handleAddFood"
-          icon="pi pi-plus"
-          severity="success"
-        />
+        <Button class="w-full" @click="handleAddFood" icon="pi pi-plus" severity="success" />
       </div>
     </div>
   </Dialog>
 
   <div>
-    <div
-      class="flex flex-row w-full items-center gap-2 justify-between p-3 border-b dark:border-b-gray-700"
-      v-for="food in history"
-      :key="food.id"
-    >
+    <div class="flex flex-row w-full items-center gap-2 justify-between p-3 border-b dark:border-b-gray-700"
+      v-for="food in history" :key="food.id">
       <div class="flex flex-col gap-2 pb-2">
-        <p class="text-lg font-bold">Food Name</p>
+        <p class="text-lg font-bold">{{ food.name }}</p>
         <p class="text-sm text-gray-500 flex gap-2">
-          <span>{{ food.name }}</span>
+          <span>{{ food.calories }} calories</span>
           -
-          <span>12 Dec, 2024</span>
+          <span>{{ food.timestamp }}</span>
         </p>
       </div>
       <div class="flex flex-row gap-2">
         <Button size="small" icon="pi pi-trash" severity="danger"></Button>
-        <Button
-          size="small"
-          icon="pi pi-pen-to-square"
-          severity="secondary"
-        ></Button>
+        <Button size="small" icon="pi pi-pen-to-square" severity="secondary"></Button>
       </div>
     </div>
   </div>
