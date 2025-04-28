4a412d42e7733404d447e39d0e8cadfbdbf6d3b0
Ashsajal <ashsajal1@gmail.com>
2025-04-28 18:59:39 +0600

    refactor: remove btns

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,25 +2,13 @@
 import { useCalorieStore } from "@/stores/calorieStore";
 import { Card, Button, Dialog, InputText, InputNumber } from "primevue";
 import { ref } from "vue";
-import { useFoodHistoryStore } from '../stores/foodHistoryStore';
+import { useFoodHistoryStore } from "../stores/foodHistoryStore";
 
 const calorieStore = useCalorieStore();
 const { history } = useFoodHistoryStore();
 const visible = ref(false);
 const foodName = ref("");
 const calories = ref(0);
-
-function add() {
-  calorieStore.addCalories(500);
-}
-
-function burn() {
-  calorieStore.burnCalories(200);
-}
-
-function reset() {
-  calorieStore.resetCalories();
-}
 </script>
 
 <template>
@@ -31,18 +19,6 @@ function reset() {
       Calories: {{ calorieStore.totalCalories }}
     </h1>
     <p>{{ calorieStore.calorieStatus }}</p>
-
-    <div class="space-x-2 mt-4">
-      <button @click="add" class="bg-green-500 px-4 py-2 text-white rounded">
-        Add 500
-      </button>
-      <button @click="burn" class="bg-red-500 px-4 py-2 text-white rounded">
-        Burn 200
-      </button>
-      <button @click="reset" class="bg-gray-500 px-4 py-2 text-white rounded">
-        Reset
-      </button>
-    </div>
   </div>
 
   <div class="p-3 flex justify-between items-center">
@@ -82,7 +58,6 @@ function reset() {
   <div>
     <div
       class="flex flex-row w-full items-center gap-2 justify-between p-3 border-b dark:border-b-gray-700"
-
       v-for="food in history"
       :key="food.id"
     >
